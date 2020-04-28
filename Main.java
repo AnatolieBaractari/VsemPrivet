7f75697ec7d010d18cd74373a482aa09f1fdafe9
AnatolieBaractari <56890293+AnatolieBaractari@users.noreply.github.com>
2020-04-28 18:30:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 package sample;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.control.Label;
@@ -10,6 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.text.Font;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -18,240 +18,188 @@ import java.util.Random;
 
 public class Main extends Application {
     private Scene scene;
-    public enum Direction {
-        UP,DOWN,RIGHT,LEFT
-    }
-    private Direction direction=Direction.RIGHT;
-    public static final int BLOCK_SIZE=30;
-    public static final int APP_W=53*BLOCK_SIZE;
-    public static final int APP_H=40*BLOCK_SIZE;
-
-
-
-    private Timeline timeline=new Timeline();
-    private Box food=new Box(30,30,30);
-    private Random r=new Random();
-    private Box[] snake=new Box[1001];
-    private Box floor=new Box(1600,1,1300);
+    private Group group;
     private Camera camera1=new PerspectiveCamera();
+    private PhongMaterial material=new PhongMaterial();
+    private AmbientLight light=new AmbientLight();
+    private Sphere [] fireballs;
+    private int fireballCounter=0;
+    private Box [] boxes;
+    private KeyFrame frame;
+    private Timeline t;
+    private Label label=new Label();
+    private int frameCounter=0;
 
-    private int intscore=0;
-    private int frames=0;
-    private Label score =new Label();
-    private int bodyIndex=0;
-    private int gotoX;
-    private int gotoZ;
-    private int thisX;
-    private int thisZ;
+    private Scene createContent() {
+        group=new Group();
+        group.getChildren().addAll(createFireballs());
+        group.getChildren().addAll(createBasis());
+        group.getChildren().addAll(createMap());
+        group.getChildren().add(createLabel());
+        scene=new Scene(group,1000,800,true);
 
-    private Scene createContent(){
-        Group root =new Group();
-
-        root.setTranslateZ(-200);
-        scene=new Scene(root,800,600,true);
+        camera1.setRotationAxis(Rotate.Y_AXIS);
+        camera1.setTranslateZ(camera1.getTranslateZ()+100);
         scene.setCamera(camera1);
-        camera1.setTranslateY(camera1.getTranslateY()-700);
-        camera1.setTranslateZ(camera1.getTranslateZ()-200);
-        camera1.setRotationAxis(Rotate.X_AXIS);
-        camera1.setRotate(-30);
 
-        floor.setTranslateY(16);
-        floor.setTranslateX(800);
-        floor.setTranslateZ(600);
-
-        food.setTranslateX((int)(Math.random()*(APP_W-BLOCK_SIZE))/BLOCK_SIZE*BLOCK_SIZE);
-        food.setTranslateZ((int)(Math.random()*(APP_H-BLOCK_SIZE))/BLOCK_SIZE*BLOCK_SIZE);
-        PhongMaterial material=new PhongMaterial(Color.LIGHTCYAN);
-        food.setMaterial(material);
-
-        PhongMaterial mat=new PhongMaterial();
-        mat.setDiffuseColor(Color.RED);
-        for(int i=1;i<1001;i++){
-            snake[i]=new Box(30,30,30);
-            if(i%2==0) {
-                snake[i].setMaterial(mat);
-            }
-            else{
-                snake[i].setMaterial(new PhongMaterial(Color.SEAGREEN));
-            }
-            snake[i].setTranslateX(3000);
-            snake[i].setTranslateZ(-i*20);
-        }
-        snake[0]=new Box(30,30,30);
-        snake[0].setMaterial(mat);
-        snake[0].setTranslateX(20);
-        snake[0].setTranslateY(0);
-
-
-        KeyFrame frame = new KeyFrame(Duration.seconds(0.1),e->{
-            frames+=1;
-            if (frames==360){
-                frames=0;
+        frame=new KeyFrame(Duration.seconds(0.016),event->{
+            frameCounter++;
+            checkFire();
+            camera1.setTranslateZ(camera1.getTranslateZ()+45);
+            if(frameCounter<400){
+                label.setTranslateZ(label.getTranslateZ()+45);
             }
 
-            gotoX=(int)(snake[0].getTranslateX());
-            gotoZ=(int)(snake[0].getTranslateZ());
-            switch (direction){
-                case UP:
-                    snake[0].setTranslateZ(snake[0].getTranslateZ()-30);
-                    break;
-
-                case DOWN:
-                    snake[0].setTranslateZ(snake[0].getTranslateZ()+30);
-                    break;
-
-                case LEFT:
-                    snake[0].setTranslateX(snake[0].getTranslateX()-30);
-                    break;
-
-                case RIGHT:
-                    snake[0].setTranslateX(snake[0].getTranslateX()+30);
-                    break;
-            }
-            for(int i=1;i<=bodyIndex;i++){
-                thisX= (int) snake[i].getTranslateX();
-                thisZ= (int) snake[i].getTranslateZ();
-                snake[i].setTranslateX(gotoX);
-                snake[i].setTranslateZ(gotoZ);
-                gotoX=thisX;
-                gotoZ=thisZ;
-            }
-            if(snake[0].getBoundsInParent().intersects(food.getBoundsInParent())){
-                food.setTranslateX((int)(Math.random()*(APP_W-BLOCK_SIZE))/BLOCK_SIZE*BLOCK_SIZE);
-                food.setTranslateZ((int)(Math.random()*(APP_H-BLOCK_SIZE))/BLOCK_SIZE*BLOCK_SIZE);
-                if(bodyIndex<1000) {
-                    intscore++;
-                    score.setText(intscore + "");
-                    bodyIndex++;
+            scene.setOnKeyPressed(e->{
+                switch (e.getCode()){
+                    case UP:
+                        camera1.setTranslateZ(camera1.getTranslateZ()+150);
+                        break;
+                    case DOWN:
+                        camera1.setTranslateZ(camera1.getTranslateZ()-50);
+                        break;
+                    case RIGHT:
+                        if(camera1.getTranslateX()<500)
+                        camera1.setTranslateX(camera1.getTranslateX()+50);
+                        break;
+                    case LEFT:
+                        if(camera1.getTranslateX()>-300)
+                        camera1.setTranslateX(camera1.getTranslateX()-50);
+                        break;
+                    case A:
+                        camera1.setRotate(camera1.getRotate()-1);
+                        break;
+                    case D:
+                        camera1.setRotate(camera1.getRotate()+1);
+                        break;
                 }
-            }
-// comment this block for auto-play
-///*
 
-            for(int i=1;i<=bodyIndex;i++){
-                if(snake[0].getTranslateX()==snake[i].getTranslateX()
-                        && snake[0].getTranslateZ()==snake[i].getTranslateZ()){
-                    restartGame();
+            });
+
+            scene.setOnMouseClicked(e->{
+
+                TranslateTransition t=new TranslateTransition(Duration.seconds(0.5),fireballs[fireballCounter]);
+                t.setCycleCount(1);
+                t.setFromX(camera1.getTranslateX()+450);
+                t.setFromZ(camera1.getTranslateZ());
+                t.setFromY(camera1.getTranslateY()+450);
+                t.setToX(e.getX());
+                t.setToZ(e.getZ());
+                t.setToY(e.getY());
+
+                FadeTransition f=new FadeTransition(Duration.seconds(0.5),fireballs[fireballCounter]);
+                f.setFromValue(1);
+                f.setToValue(0);
+                f.setCycleCount(1);
+
+                t.play();
+                f.play();
+
+                if(fireballCounter>=9){
+                    fireballCounter=0;
                 }
-            }
-
-
-            if(snake[0].getTranslateX()<0 && direction==Direction.LEFT){
-                snake[0].setTranslateX(APP_W);
-            }
-            if (snake[0].getTranslateX()>APP_W && direction==Direction.RIGHT){
-                snake[0].setTranslateX(0);
-            }
-
-            if(snake[0].getTranslateZ()<0 && direction==Direction.UP){
-                snake[0].setTranslateZ(APP_H);
-            }
-            if (snake[0].getTranslateZ()>APP_H && direction==Direction.DOWN){
-                snake[0].setTranslateZ(0);
-            }
-
-
+                else {
+                    fireballCounter++;
+                }
+            });
         });
 
-        timeline.getKeyFrames().add(frame);
-        timeline.setCycleCount(Animation.INDEFINITE);
+        t=new Timeline();
+        t.setCycleCount(Animation.INDEFINITE);
+        t.getKeyFrames().add(frame);
 
-        for(int i=0;i<1001;i++){
-            root.getChildren().add(snake[i]);
-        }
-        root.getChildren().addAll(food,floor);
-
-        scene.setOnKeyPressed(e->{
-            switch (e.getCode()) {
-                case S:
-                case DOWN:
-                    if (direction != Direction.DOWN) {
-                        direction = Direction.UP;
-                    }
-                    break;
-                case W:
-                case UP:
-                    if (direction != Direction.UP) {
-                        direction = Direction.DOWN;
-                    }
-                    break;
-                case A:
-                case LEFT:
-                    if (direction != Direction.RIGHT) {
-                        direction = Direction.LEFT;
-                    }
-                    break;
-                case D:
-                case RIGHT:
-                    if (direction != Direction.LEFT) {
-                        direction = Direction.RIGHT;
-                    }
-                    break;
-
-                case Q:
-                    int max = bodyIndex + 10;
-                    for (int i = bodyIndex; i <= (max); i++) {
-                        intscore++;
-                        score.setText(intscore * 69 + "");
-                        bodyIndex++;
-                        if(bodyIndex%2==0) {
-                            snake[bodyIndex].setMaterial(new PhongMaterial(Color.RED));
-                        }
-                        else{
-                            snake[bodyIndex].setMaterial(new PhongMaterial(Color.SEAGREEN));
-                        }
-                    }
-                    break;
-                case R:
-                    if(bodyIndex>10){
-                        int min = bodyIndex - 10;
-                        for (int i = bodyIndex + 1; i >=(min); i--) {
-                            intscore--;
-                            score.setText(intscore * 69 + "");
-                            snake[bodyIndex].setMaterial(new PhongMaterial(Color.TRANSPARENT));
-                            bodyIndex--;
-                            snake[bodyIndex].setTranslateY(-1000);
-                        }
-                    }
-
-                    break;
-
-            }
-        });
         return scene;
     }
 
-
-    private void restartGame(){
-        stopGame();
-        startGame();
+    private Label createLabel() {
+        label=new Label();
+        label.setText("Click to shoot\nPress UP to accelerate\nPress DOWN to slow down\nPress LEFT to go left\nPress RIGHT to go right\nPress A to rotate to left\nPress D to rotate to right\n");
+        label.setFont(Font.font(50));
+        label.setTextFill(Color.BLUE);
+        label.setTranslateX(300);
+        label.setTranslateY(0);
+        label.setTranslateZ(2500);
+        return label;
     }
 
-    private void stopGame(){
-        timeline.stop();
-        for(int i=1;i<=bodyIndex;i++){
-            snake[i].setTranslateY(-10000);
-            snake[i].setTranslateZ(-2000);
-            intscore=0;
+    private void checkFire() {
+        for(int i=0;i<10;i++){
+            for(int j=0;j<200;j++){
+                if(fireballs[i].getBoundsInParent().intersects(boxes[j].getBoundsInParent())){
+                    boxes[j].setMaterial(new PhongMaterial(Color.TRANSPARENT));
+                }
+            }
         }
     }
 
-    private void startGame(){
-        bodyIndex=0;
-        snake[0].setTranslateY(0);
-        snake[0].setTranslateX(20);
-        direction= Direction.RIGHT;
-        timeline.play();
+
+    private Sphere[] createFireballs() {
+        PhongMaterial material=new PhongMaterial();
+        material.setDiffuseMap(new Image("fireball.png"));
+        fireballs=new Sphere[10];
+        for(int i=0;i<10;i++){
+            fireballs[i]=new Sphere(80);
+            fireballs[i].setMaterial(material);
+        }
+        return fireballs;
     }
+
+    private Box[] createBasis() {
+        PhongMaterial material1=new PhongMaterial(Color.LIGHTCORAL);
+        material1.setSelfIlluminationMap(new Image("illumination1.png"));
+        Box [] uno =new Box[2];
+        uno[0]=new Box(4000,10,700000);
+        uno[0].setTranslateY(500);
+        uno[0].setTranslateX(460);
+        uno[1]=new Box(4000,10,700000);
+        uno[1].setTranslateY(-100);
+        uno[1].setTranslateX(460);
+        for (int i=0;i<2;i++){
+            uno[i].setMaterial(material1);
+        }
+       return uno;
+    }
+
+    private Box[] createMap() {
+        material.setDiffuseColor(Color.LIGHTBLUE);
+        Random r1=new Random();
+        boxes=new Box[1000];
+        double sum1=20000,sum2=20000;
+        for(int i=0;i<1000;i++){
+            int red=r1.nextInt(5)+1;
+            double r= r1.nextInt(2)+1;
+            if(i%2==0){
+                boxes[i]=new Box(r*120,r*150,r*600);
+                boxes[i].setTranslateZ(sum1);
+                boxes[i].setTranslateX(0);
+                boxes[i].setTranslateY(300);
+                sum1+=(r*2000)+200;
+            }
+            else if(i%2==1){
+                boxes[i]=new Box(r*120,r*150,r*600);
+                boxes[i].setTranslateZ(sum2);
+                boxes[i].setTranslateX(1200);
+                boxes[i].setTranslateY(300);
+                sum2+=(r*2000)+200;
+            }
+            if(red==1){
+                boxes[i].setMaterial(new PhongMaterial(Color.RED));
+            }
+            else
+                boxes[i].setMaterial(material);
+
+        }
+      return boxes;
+    }
+
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(createContent());
-        stage.show();
-        timeline.play();
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setScene(createContent());
+        primaryStage.show();
+        t.play();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
